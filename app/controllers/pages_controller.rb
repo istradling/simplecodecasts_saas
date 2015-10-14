6154bab7a360e65a4febb3153235543f258146cb
Ian Stradling <i_strad@yahoo.com>
2015-10-14 21:17:55 +0000

    added about page view, action, route

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,5 +4,8 @@ class PagesController < ApplicationController
         
     end
     
+    def about
+    end
+    
     
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   
+  
+  
+  
+  get '/about' => 'pages#about'
   root 'pages#home'
   
   
